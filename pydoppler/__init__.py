4f1a86b238fb325729e16bf2437e55f1e4ffe0a8
zeroday0619 <zeroday0619_dev@outlook.com>
2022-07-21 11:45:44 +0900

    chore: refactor code

--- pydoppler/__init__.py
+++ pydoppler/__init__.py
@@ -6,8 +6,10 @@ The full copyright notice can be found in the file LICENSE.
 __version__ = "0.1.0"
 
 from pydoppler.doppler import Doppler as _Doppler
+from pydoppler.doppler import DopplerAudit as _DopplerAudit
 
 
 Doppler = _Doppler
+DopplerAudit = _DopplerAudit
 
-__all__ = ["Doppler"]
+__all__ = ["Doppler", "DopplerAudit"]
